2dffd8310ff83de9568b5c1e1affac236acf95f7
Dirc <e.e.cornet@gmail.com>
2018-07-30 15:57:40 +0200

    add slave public_ips to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "master.ip" {
+output "master_ip" {
   value = "${aws_instance.master.public_ip}"
+}
+
+output "slave_ips" {
+  value = ["${aws_instance.slave.*.public_ip}"]
+}
+
+output "all_ips" {
+  value = ["${aws_instance.master.public_ip}","${aws_instance.slave.*.public_ip}"]
 }
